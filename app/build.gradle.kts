124ed276468354ff371e5c631fbb0f886f08336e
Axel Santivañez <asantivanezg@hotmail.com>
2025-04-13 22:39:06 -0500

    removed secrets gradle dependency, added room, kotlin serialization, ksp, viewmodel compose, navigation hilt, okhttp dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt.plugin)
-    alias(libs.plugins.secrets.gradle.plugin)
-    kotlin("kapt")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -26,6 +26,14 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        debug {
+            val baseUrl = "https://api.themoviedb.org"
+            buildConfigField("String", "BASE_URL", "\"$baseUrl\"")
+            val token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZjA0NDYwMmY3YjAwZjhiMDE2MGMyZTFmYTViMTZjNiIsIm5iZiI6MTU0OTA2MDc0Ni44NDUsInN1YiI6IjVjNTRjYThhYzNhMzY4MGI1Nzg0NDRjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D_w_Yb4TOLRYlS_onBW4k8AXBrtuiNZWKbb6iGVp3II"
+            buildConfigField("String", "TOKEN", "\"$token\"")
+            val apiKey = "bf044602f7b00f8b0160c2e1fa5b16c6"
+            buildConfigField("String", "API_KEY", "\"$apiKey\"")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -36,6 +44,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -49,6 +58,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewModel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     // coil
     implementation(libs.coil)
@@ -56,18 +68,28 @@ dependencies {
 
     // retrofit
     implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
     implementation(libs.gson)
     implementation(libs.gson.converter)
 
+    // room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.room.paging)
+    ksp(libs.room.compiler)
+
     // hilt
     implementation(libs.hilt.android)
-    kapt(libs.hilt.kpt)
+    implementation(libs.hilt.navigation.android)
+    ksp(libs.hilt.kpt)
 
     // paging3
     implementation(libs.paging)
     implementation(libs.paging.runtime)
 
     implementation(libs.kotlin.coroutine)
+    implementation(libs.kotlin.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
